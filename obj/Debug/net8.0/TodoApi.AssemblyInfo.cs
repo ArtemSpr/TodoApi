6f5750f089b20cf22e68b40d261ad12d1a65be4b
ArtemSpr <artemspravnikov77@gmail.com>
2025-09-08 15:45:27 +0300

    Modify launch.json and task.json

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e7c6a274457c12b0259ac1cdc09d04e4a3e2ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46f869751614ac8590b45ee5c3918fc0a7939b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
